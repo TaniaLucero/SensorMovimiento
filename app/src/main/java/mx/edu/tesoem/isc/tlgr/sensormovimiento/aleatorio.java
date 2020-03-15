03a1aa9d48bfda80e450ebc63eb4190cf85e6067
Tania Lucero <tanialucerorangel@gmail.com>
2020-03-14 20:32:48 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/tlgr/sensormovimiento/aleatorio.java
@@ -0,0 +1,21 @@
+package mx.edu.tesoem.isc.tlgr.sensormovimiento;
+
+public class aleatorio {
+    int Imagen, sonido;
+    String aleatorio;
+
+
+    public aleatorio(int Imagen,String aleatorio, int sonido){
+        this.Imagen = Imagen;
+        this.aleatorio = aleatorio;
+        this.sonido = sonido;
+    }
+
+    public int getImagen() { return Imagen; }
+
+    public String getAleatorio() { return aleatorio; }
+
+    public int getSonido() {
+        return sonido;
+    }
+}
